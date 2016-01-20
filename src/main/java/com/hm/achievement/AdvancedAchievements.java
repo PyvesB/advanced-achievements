2333bf8dc81e66fb49c7bc3123d9de5416cfc819
DarkPyves <deeprevenge1@gmail.com>
2016-01-20 23:42:41 +0000

    Prepared 2.3.2 release
    Prepared 2.3.2 release.

--- src/main/java/com/hm/achievement/AdvancedAchievements.java
+++ src/main/java/com/hm/achievement/AdvancedAchievements.java
@@ -62,7 +62,7 @@ import com.hm.achievement.runnable.AchievePlayTimeRunnable;
  * Spigot project page: spigotmc.org/resources/advanced-achievements.6239
  * 
  * @since April 2015
- * @version 2.3.1
+ * @version 2.3.2
  * @author DarkPyves
  */
 
