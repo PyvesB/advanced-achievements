85cb6eaefaefe9ae143bf26cd70ec3d2b1e20341
DarkPyves <deeprevenge1@gmail.com>
2016-11-30 20:50:53 +0000

    Fixed #105

--- src/main/java/com/hm/achievement/listener/AchieveTradeAnvilBrewSmeltListener.java
+++ src/main/java/com/hm/achievement/listener/AchieveTradeAnvilBrewSmeltListener.java
@@ -1,5 +1,7 @@
 package com.hm.achievement.listener;
 
+import java.util.HashMap;
+
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,6 +10,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.ItemStack;
 
 import com.hm.achievement.AdvancedAchievements;
 import com.hm.achievement.category.NormalAchievements;
@@ -34,20 +37,15 @@ public class AchieveTradeAnvilBrewSmeltListener extends AbstractListener impleme
 		}
 
 		Player player = (Player) event.getWhoClicked();
-		if (!shouldEventBeTakenIntoAccountNoPermission(player)
-				|| event.isShiftClick() && player.getInventory().firstEmpty() < 0) {
-			return;
-		}
-
 		NormalAchievements category;
-		InventoryType inventoryMaterial = event.getInventory().getType();
-		if (event.getRawSlot() == 2 && inventoryMaterial == InventoryType.MERCHANT) {
+		InventoryType inventoryType = event.getInventory().getType();
+		if (event.getRawSlot() == 2 && inventoryType == InventoryType.MERCHANT) {
 			category = NormalAchievements.TRADES;
-		} else if (event.getRawSlot() == 2 && inventoryMaterial == InventoryType.ANVIL) {
+		} else if (event.getRawSlot() == 2 && inventoryType == InventoryType.ANVIL) {
 			category = NormalAchievements.ANVILS;
-		} else if (inventoryMaterial == InventoryType.BREWING) {
+		} else if (inventoryType == InventoryType.BREWING) {
 			category = NormalAchievements.BREWING;
-		} else if (inventoryMaterial == InventoryType.FURNACE) {
+		} else if (inventoryType == InventoryType.FURNACE) {
 			category = NormalAchievements.SMELTING;
 		} else {
 			return;
@@ -62,6 +60,29 @@ public class AchieveTradeAnvilBrewSmeltListener extends AbstractListener impleme
 			return;
 		}
 
-		updateStatisticAndAwardAchievementsIfAvailable(player, category, event.getCurrentItem().getAmount());
+		int eventAmount = event.getCurrentItem().getAmount();
+		// Case where the player uses shift + click but there are no empty slots in his inventory. Part or all of the
+		// items may still be successfully retrieved if the player's inventory has some non full stacks corresponding to
+		// the exact same ItemStack.
+		if (event.isShiftClick() && player.getInventory().firstEmpty() < 0) {
+			int availableSpaces = 0;
+			Material eventMaterial = event.getCurrentItem().getType();
+			// Get all similar ItemStacks in the player's inventory.
+			HashMap<Integer, ? extends ItemStack> inventoryEventItemStackMap = player.getInventory().all(eventMaterial);
+			// For each of these stacks, add their remaining space.
+			for (ItemStack itemStack : inventoryEventItemStackMap.values()) {
+				availableSpaces += (eventMaterial.getMaxStackSize() - itemStack.getAmount());
+			}
+			// Not enough space available to fit everything: the statistic will only increase by the number of items
+			// actually retrieved in the inventory during this action.
+			if (availableSpaces < eventAmount) {
+				eventAmount = availableSpaces;
+			}
+			if (eventAmount == 0) {
+				return;
+			}
+		}
+
+		updateStatisticAndAwardAchievementsIfAvailable(player, category, eventAmount);
 	}
 }
