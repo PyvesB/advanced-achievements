f540ef1fe48ad9b5c902c924c12aca2bcf9c4d84
PyvesB <deeprevenge1@gmail.com>
2017-03-18 21:36:41 +0000

    Reverted "Fixed permissions for categories using metadata information"
    This reverts commit a24dbde9667a8a2e392b78870473475a4badcf3c.

--- src/main/java/com/hm/achievement/listener/AchieveBlockBreakListener.java
+++ src/main/java/com/hm/achievement/listener/AchieveBlockBreakListener.java
@@ -62,13 +62,16 @@ public class AchieveBlockBreakListener extends AbstractListener implements Liste
 		MultipleAchievements category = MultipleAchievements.BREAKS;
 
 		String blockName = block.getType().name().toLowerCase();
-		if (!player.hasPermission(category.toPermName() + '.' + blockName)) {
-			return;
-		}
-		if (plugin.getPluginConfig().isConfigurationSection(category + "." + blockName + ':' + block.getData())) {
+		if (player.hasPermission(category.toPermName() + '.' + blockName + '.' + block.getData()) && plugin
+				.getPluginConfig().isConfigurationSection(category + "." + blockName + ':' + block.getData())) {
 			blockName += ":" + block.getData();
-		} else if (!plugin.getPluginConfig().isConfigurationSection(category + "." + blockName)) {
-			return;
+		} else {
+			if (!player.hasPermission(category.toPermName() + '.' + blockName)) {
+				return;
+			}
+			if (!plugin.getPluginConfig().isConfigurationSection(category + "." + blockName)) {
+				return;
+			}
 		}
 
 		updateStatisticAndAwardAchievementsIfAvailable(player, category, blockName, 1);
