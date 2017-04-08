3751090c433944ee7d125bcc1fbca75e58bd13bf
PyvesB <deeprevenge1@gmail.com>
2017-04-08 12:07:27 +0100

    Increased version number to 5.0.2

--- src/main/java/com/hm/achievement/AdvancedAchievements.java
+++ src/main/java/com/hm/achievement/AdvancedAchievements.java
@@ -99,7 +99,7 @@ import codecrafter47.bungeetablistplus.api.bukkit.BungeeTabListPlusBukkitAPI;
  * Spigot project page: spigotmc.org/resources/advanced-achievements.6239
  * 
  * @since April 2015
- * @version 5.0.1
+ * @version 5.0.2
  * @author Pyves
  */
 public class AdvancedAchievements extends JavaPlugin {
