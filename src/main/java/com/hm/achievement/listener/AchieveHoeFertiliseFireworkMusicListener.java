774b321f5a59d3b5412a8f6df02b61da6bffee58
PyvesB <PyvesDev@gmail.com>
2017-05-13 11:13:35 +0100

    Fixed #187 and other related abuses

--- src/main/java/com/hm/achievement/listener/AchieveHoeFertiliseFireworkMusicListener.java
+++ src/main/java/com/hm/achievement/listener/AchieveHoeFertiliseFireworkMusicListener.java
@@ -42,7 +42,7 @@ public class AchieveHoeFertiliseFireworkMusicListener extends AbstractRateLimite
 		} else if (event.getItem().isSimilar(new ItemStack(Material.INK_SACK, 1, (short) 15))
 				&& canBeFertilised(clickedMaterial, event.getClickedBlock())) {
 			category = NormalAchievements.FERTILISING;
-		} else if (event.getItem().getType() == Material.FIREWORK) {
+		} else if (event.getItem().getType() == Material.FIREWORK && canAccommodateFireworkLaunch(clickedMaterial)) {
 			category = NormalAchievements.FIREWORKS;
 		} else if (event.getItem().getType().isRecord() && clickedMaterial == Material.JUKEBOX) {
 			category = NormalAchievements.MUSICDISCS;
@@ -88,5 +88,115 @@ public class AchieveHoeFertiliseFireworkMusicListener extends AbstractRateLimite
 				|| clickedMaterial == Material.BROWN_MUSHROOM || clickedMaterial == Material.RED_MUSHROOM
 				|| clickedMaterial == Material.COCOA && durability < 9 || clickedMaterial == Material.LONG_GRASS
 				|| (version >= 9 && clickedMaterial == Material.BEETROOT_BLOCK && durability < 3);
+	}
+
+	/**
+	 * Determines whether a firework can be launched when interacting with this block.
+	 * 
+	 * @param clickedMaterial
+	 * @return
+	 */
+	private boolean canAccommodateFireworkLaunch(Material clickedMaterial) {
+		switch (clickedMaterial) {
+			case WORKBENCH:
+			case FURNACE:
+			case BURNING_FURNACE:
+			case DISPENSER:
+			case CHEST:
+			case NOTE_BLOCK:
+			case LEVER:
+			case STONE_BUTTON:
+			case WOOD_BUTTON:
+			case TRAP_DOOR:
+			case FENCE_GATE:
+			case ENCHANTMENT_TABLE:
+			case ENDER_CHEST:
+			case BEACON:
+			case ANVIL:
+			case TRAPPED_CHEST:
+			case HOPPER:
+			case DROPPER:
+			case WOOD_DOOR:
+			case WOODEN_DOOR:
+			case PAINTING:
+			case MINECART:
+			case HOPPER_MINECART:
+			case EXPLOSIVE_MINECART:
+			case COMMAND_MINECART:
+			case POWERED_MINECART:
+			case STORAGE_MINECART:
+			case BOAT:
+			case BED_BLOCK:
+			case BREWING_STAND:
+			case CAKE_BLOCK:
+			case ITEM_FRAME:
+			case REDSTONE_COMPARATOR_OFF:
+			case REDSTONE_COMPARATOR_ON:
+			case DIODE_BLOCK_OFF:
+			case DIODE_BLOCK_ON:
+			case COMMAND:
+				return false;
+			default:
+				break;
+		}
+
+		if (version >= 8) {
+			switch (clickedMaterial) {
+				case ACACIA_FENCE_GATE:
+				case BIRCH_FENCE_GATE:
+				case DARK_OAK_FENCE_GATE:
+				case JUNGLE_FENCE_GATE:
+				case SPRUCE_FENCE_GATE:
+				case ACACIA_DOOR:
+				case BIRCH_DOOR:
+				case DARK_OAK_DOOR:
+				case JUNGLE_DOOR:
+				case SPRUCE_DOOR:
+				case ARMOR_STAND:
+					return false;
+				default:
+					break;
+			}
+		}
+
+		if (version >= 9) {
+			switch (clickedMaterial) {
+				case BOAT_ACACIA:
+				case BOAT_BIRCH:
+				case BOAT_DARK_OAK:
+				case BOAT_JUNGLE:
+				case BOAT_SPRUCE:
+				case COMMAND_REPEATING:
+				case COMMAND_CHAIN:
+					return false;
+				default:
+					break;
+			}
+		}
+
+		if (version >= 11) {
+			switch (clickedMaterial) {
+				case BLACK_SHULKER_BOX:
+				case BLUE_SHULKER_BOX:
+				case BROWN_SHULKER_BOX:
+				case CYAN_SHULKER_BOX:
+				case GRAY_SHULKER_BOX:
+				case GREEN_SHULKER_BOX:
+				case LIGHT_BLUE_SHULKER_BOX:
+				case LIME_SHULKER_BOX:
+				case MAGENTA_SHULKER_BOX:
+				case ORANGE_SHULKER_BOX:
+				case PINK_SHULKER_BOX:
+				case PURPLE_SHULKER_BOX:
+				case RED_SHULKER_BOX:
+				case SILVER_SHULKER_BOX:
+				case WHITE_SHULKER_BOX:
+				case YELLOW_SHULKER_BOX:
+					return false;
+				default:
+					break;
+			}
+		}
+		return true;
 	}
 }
