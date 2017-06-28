5e9767af2cae3d9b9ee211b2b63b4ba0709392ca
PyvesB <PyvesDev@gmail.com>
2017-06-28 21:10:04 +0100

    Fixed firework damage cancellation only affecting one entity

--- src/main/java/com/hm/achievement/listener/FireworkListener.java
+++ src/main/java/com/hm/achievement/listener/FireworkListener.java
@@ -32,7 +32,7 @@ public class FireworkListener extends AbstractListener {
 	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
 	public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
 		Entity damager = event.getDamager();
-		if (damager != null && fireworksLaunchedByPlugin.remove(damager.getUniqueId().toString())) {
+		if (damager != null && fireworksLaunchedByPlugin.contains(damager.getUniqueId().toString())) {
 			event.setCancelled(true);
 		}
 	}
@@ -41,7 +41,7 @@ public class FireworkListener extends AbstractListener {
 		final String uuid = firework.getUniqueId().toString();
 		fireworksLaunchedByPlugin.add(uuid);
 
-		// Schedule for removal to avoid creating memory leaks if firework does not damage player.
+		// Schedule for removal to avoid creating memory leaks.
 		Bukkit.getServer().getScheduler().runTaskLaterAsynchronously(plugin, new Runnable() {
 
 			@Override
