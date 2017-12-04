8a937fb777adc4a29cba4c54d2765d7b9bdb498f
Phoetrix <dylan.phoetrix@gmail.com>
2017-12-04 22:21:19 +0100

    Update AchievementPlaceholderHook.java

--- src/main/java/com/hm/achievement/utils/AchievementPlaceholderHook.java
+++ src/main/java/com/hm/achievement/utils/AchievementPlaceholderHook.java
@@ -9,49 +9,44 @@ import com.hm.achievement.category.NormalAchievements;
 import me.clip.placeholderapi.external.EZPlaceholderHook;
 
 /**
- * Class enabling usage of placeholder with PlaceholderAPI to get achievements stats in others plugins.
+ * Class enabling usage of placeholder with PlaceholderAPI to get achievements
+ * stats in others plugins.
  * 
  * @author Phoetrix
  *
  */
-public class AchievementPlaceholderHook extends EZPlaceholderHook
-{
+public class AchievementPlaceholderHook extends EZPlaceholderHook {
 	private final AdvancedAchievements plugin;
-	
-	public AchievementPlaceholderHook(AdvancedAchievements plugin)
-	{
+
+	public AchievementPlaceholderHook(AdvancedAchievements plugin) {
 		super(plugin, "aach");
 		this.plugin = plugin;
 	}
-	
+
 	@Override
-	public String onPlaceholderRequest(Player p, String identifier)
-	{
-		for (NormalAchievements category : NormalAchievements.values())
-		{
-			String categoryName = category.toString();
-			
-			if (identifier.equalsIgnoreCase(categoryName))
-			{
-				return String.valueOf(plugin.getCacheManager().getAndIncrementStatisticAmount(category, p.getUniqueId(), 0));
-			}
-		}
-		
-		for (MultipleAchievements category : MultipleAchievements.values())
-		{
+	public String onPlaceholderRequest(Player p, String identifier) {
+		for (NormalAchievements category : NormalAchievements.values()) {
 			String categoryName = category.toString();
 
-			for (String subcategory : plugin.getPluginConfig().getConfigurationSection(categoryName).getKeys(false))
-			{
-				String categoryPath = categoryName+"_"+subcategory;
-				
-				if (identifier.equalsIgnoreCase(categoryPath))
-				{
-					return String.valueOf(plugin.getCacheManager().getAndIncrementStatisticAmount(category, subcategory, p.getUniqueId(), 0));
+			if (identifier.equalsIgnoreCase(categoryName)) {
+				return String
+						.valueOf(plugin.getCacheManager().getAndIncrementStatisticAmount(category, p.getUniqueId(), 0));
+			}
+		}
+
+		for (MultipleAchievements category : MultipleAchievements.values()) {
+			String categoryName = category.toString();
+
+			for (String subcategory : plugin.getPluginConfig().getConfigurationSection(categoryName).getKeys(false)) {
+				String categoryPath = categoryName + "_" + subcategory;
+
+				if (identifier.equalsIgnoreCase(categoryPath)) {
+					return String.valueOf(plugin.getCacheManager().getAndIncrementStatisticAmount(category, subcategory,
+							p.getUniqueId(), 0));
 				}
 			}
 		}
-		
+
 		return null;
 	}
 }
