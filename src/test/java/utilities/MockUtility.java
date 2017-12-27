3412beee5875cb26261066523a24d6ff8ba825ce
Rsl1122 <djrapitops@hotmail.com>
2017-12-27 10:46:55 +0200

    Moved to standard maven test directory

---
+++ src/test/java/utilities/MockUtility.java
@@ -0,0 +1,76 @@
+package java.utilities;
+
+import com.hm.achievement.AdvancedAchievements;
+import com.hm.mcshared.file.CommentedYamlConfiguration;
+import org.bukkit.plugin.InvalidDescriptionException;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.powermock.api.mockito.PowerMockito;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.logging.Logger;
+
+import static org.powermock.api.mockito.PowerMockito.when;
+
+/**
+ * Utility class for Mocking plugin components prior tests in order to complete testing.
+ *
+ * @author Rsl1122
+ */
+public class MockUtility {
+
+    private AdvancedAchievements pluginMock;
+
+    private MockUtility() {
+
+    }
+
+    public static MockUtility setUp() {
+        return new MockUtility().initializePluginMock();
+    }
+
+    public MockUtility initializePluginMock() {
+        pluginMock = PowerMockito.mock(AdvancedAchievements.class);
+        return this;
+    }
+
+    public MockUtility mockLogger() {
+        Logger testLogger = Logger.getLogger("TestLogger");
+        when(pluginMock.getLogger()).thenReturn(testLogger);
+        return this;
+    }
+
+    private void mockResourceFetching() throws Exception {
+        mockPluginDescription();
+        mockDataFolder();
+        File configYml = new File(getClass().getResource("/config.yml").getPath());
+        when(pluginMock.getResource("config.yml")).thenReturn(new FileInputStream(configYml));
+    }
+
+    private void mockDataFolder() {
+        TestFolder.clearFolder();
+        TestFolder.createFolder();
+        File folder = TestFolder.getFolder();
+        when(pluginMock.getDataFolder()).thenReturn(folder);
+    }
+
+    private void mockPluginDescription() throws InvalidDescriptionException, FileNotFoundException {
+        File pluginYml = new File(getClass().getResource("/plugin.yml").getPath());
+        PluginDescriptionFile desc = new PluginDescriptionFile(new FileInputStream(pluginYml));
+        when(pluginMock.getDescription()).thenReturn(desc);
+    }
+
+    public MockUtility mockPluginConfig() throws Exception {
+        mockResourceFetching();
+        mockPluginDescription();
+
+        CommentedYamlConfiguration config = new CommentedYamlConfiguration("config.yml", pluginMock);
+        when(pluginMock.getPluginConfig()).thenReturn(config);
+        return this;
+    }
+
+    public AdvancedAchievements getPluginMock() {
+        return pluginMock;
+    }
+}
---
+++ src/test/java/utilities/TestFolder.java
@@ -0,0 +1,56 @@
+package java.utilities;
+
+import java.io.File;
+import java.util.Objects;
+
+/**
+ * Class responsible for clearing and creating a temporary test folder.
+ *
+ * @author Rsl1122
+ */
+public class TestFolder {
+
+    private static final String path = getPath();
+
+    private static String getPath() {
+        File configFile = new File(TestFolder.class.getResource("/config.yml").getPath());
+        File targetFolder = configFile.getParentFile().getParentFile();
+        File testFolder = new File(targetFolder, "test-folder");
+        return testFolder.getPath();
+    }
+
+    public static boolean clearFolder() {
+        System.out.println("Clearing Folder: " + path);
+        File folder = getFolder();
+        if (folder.exists()) {
+            return clear(folder);
+        }
+        return true;
+    }
+
+    public static File getFolder() {
+        return new File(path);
+    }
+
+    public static boolean createFolder() {
+        System.out.println("Creating Folder: " + path);
+        File folder = getFolder();
+        return folder.mkdirs();
+    }
+
+    /**
+     * Delete files recursively.
+     *
+     * @param file File or Folder to delete
+     * @return
+     */
+    private static boolean clear(File file) {
+        if (file.isDirectory()) {
+            for (File subFile : Objects.requireNonNull(file.listFiles())) {
+                clear(subFile);
+            }
+        }
+        return file.delete();
+    }
+
+}
