6df5ead37f49d1ce037e078694b46a76124322f9
Rsl1122 <djrapitops@hotmail.com>
2018-01-01 19:14:02 +0200

    Added data objects for Achievement returned by database.

---
+++ src/main/java/com/hm/achievement/db/data/Achievement.java
@@ -0,0 +1,55 @@
+package com.hm.achievement.db.data;
+
+import com.hm.achievement.category.Category;
+
+/**
+ * Data container object for a single achievement.
+ *
+ * The object was created to reduce "messy lists" returned by the database.
+ */
+public class Achievement {
+
+    private String name;
+    private String message;
+
+    private Category category;
+
+    public Achievement(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+
+    public Achievement(String name, String message, Category category) {
+        this.name = name;
+        this.message = message;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public boolean hasBeenAwarded() {
+        return this instanceof AwardedAchievement;
+    }
+}
---
+++ src/main/java/com/hm/achievement/db/data/AwardedAchievement.java
@@ -0,0 +1,35 @@
+package com.hm.achievement.db.data;
+
+import com.hm.achievement.category.Category;
+
+import java.sql.Date;
+import java.util.UUID;
+
+/**
+ * Represents an Achievement that has been awarded to a player.
+ */
+public class AwardedAchievement extends Achievement {
+
+    private final UUID awardedTo;
+    private final Date dateAwarded;
+
+    public AwardedAchievement(UUID awardedTo, String name, String message, Date dateAwarded) {
+        super(name, message);
+        this.awardedTo = awardedTo;
+        this.dateAwarded = dateAwarded;
+    }
+
+    public AwardedAchievement(UUID awardedTo, String name, String message, Date dateAwarded, Category category) {
+        super(name, message, category);
+        this.awardedTo = awardedTo;
+        this.dateAwarded = dateAwarded;
+    }
+
+    public UUID getAwardedTo() {
+        return awardedTo;
+    }
+
+    public Date getDateAwarded() {
+        return dateAwarded;
+    }
+}
