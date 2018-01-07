a2e83690371aa1fa1e29127973c07c6e7277137f
Rsl1122 <djrapitops@hotmail.com>
2018-01-07 17:19:08 +0200

    getCleanPlayerAchievementsList to Tests. (Replaced spaces with tabs in tests)

--- src/test/java/com/hm/achievement/db/SQLiteDatabaseBasicTest.java
+++ src/test/java/com/hm/achievement/db/SQLiteDatabaseBasicTest.java
@@ -2,6 +2,7 @@ package com.hm.achievement.db;
 
 import com.google.common.util.concurrent.MoreExecutors;
 import com.hm.achievement.AdvancedAchievements;
+import com.hm.achievement.db.data.AwardedDBAchievement;
 import com.hm.achievement.exception.PluginLoadError;
 import org.junit.*;
 import org.junit.rules.TemporaryFolder;
@@ -22,192 +23,193 @@ import static org.junit.Assert.*;
 @RunWith(MockitoJUnitRunner.class)
 public class SQLiteDatabaseBasicTest extends SQLiteDatabaseTest {
 
-    @ClassRule
-    public static final TemporaryFolder temporaryFolder = new TemporaryFolder();
+	@ClassRule
+	public static final TemporaryFolder temporaryFolder = new TemporaryFolder();
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-        MockUtility mockUtility = MockUtility.setUp()
-                .mockLogger()
-                .mockDataFolder(temporaryFolder.getRoot())
-                .mockPluginConfig();
-        AdvancedAchievements pluginMock = mockUtility.getPluginMock();
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		MockUtility mockUtility = MockUtility.setUp()
+				.mockLogger()
+				.mockDataFolder(temporaryFolder.getRoot())
+				.mockPluginConfig();
+		AdvancedAchievements pluginMock = mockUtility.getPluginMock();
 
-        db = new SQLiteDatabaseManager(pluginMock) {
-            @Override
-            public void extractConfigurationParameters() {
-                super.extractConfigurationParameters();
-                pool = MoreExecutors.newDirectExecutorService();
-            }
-        };
-        initDB();
-    }
+		db = new SQLiteDatabaseManager(pluginMock) {
+			@Override
+			public void extractConfigurationParameters() {
+				super.extractConfigurationParameters();
+				pool = MoreExecutors.newDirectExecutorService();
+			}
+		};
+		initDB();
+	}
 
-    @Before
-    public void setUp() {
-        clearDatabase();
-    }
+	@Before
+	public void setUp() {
+		clearDatabase();
+	}
 
-    @AfterClass
-    public static void tearDownClass() {
-        if (db != null) {
-            db.shutdown();
-        }
-    }
+	@AfterClass
+	public static void tearDownClass() {
+		if (db != null) {
+			db.shutdown();
+		}
+	}
 
-    @Test
-    public void testInitialization() throws PluginLoadError {
-        initDB();
-    }
+	@Test
+	public void testInitialization() throws PluginLoadError {
+		initDB();
+	}
 
-    @Test
-    public void testGetAchievementList() {
-        registerAchievement();
+	@Test
+	public void testGetAchievementList() {
+		registerAchievement();
 
-        List<String> achievements = db.getPlayerAchievementsList(testUUID);
-        assertEquals(3, achievements.size());
+		List<AwardedDBAchievement> achievements = db.getCleanPlayerAchievementsList(testUUID);
+		assertEquals(1, achievements.size());
+		AwardedDBAchievement found = achievements.get(0);
+		AwardedDBAchievement expected = 
+				new AwardedDBAchievement(testUUID, testAchievement, testAchievementMsg, found.getDateAwarded(), found.getFormattedDate());
+		assertEquals(expected, found);
+	}
 
-        List<String> expected = Arrays.asList(testAchievement, testAchievementMsg, achievements.get(2));
-        assertEquals(expected, achievements);
-    }
-
-    @Test
-    public void testAchievementCount() {
-        registerAchievement();
+	@Test
+	public void testAchievementCount() {
+		registerAchievement();
 
 
-        Map<UUID, Integer> expected = Collections.singletonMap(testUUID, 1);
+		Map<UUID, Integer> expected = Collections.singletonMap(testUUID, 1);
 
-        Map<UUID, Integer> actual = db.getPlayersAchievementsAmount();
-        assertEquals(expected, actual);
-    }
+		Map<UUID, Integer> actual = db.getPlayersAchievementsAmount();
+		assertEquals(expected, actual);
+	}
 
-    @Test
-    public void testAchievementDateRegistration() {
-        String date = db.getPlayerAchievementDate(testUUID, testAchievement);
-        assertNull(date);
+	@Test
+	public void testAchievementDateRegistration() {
+		String date = db.getPlayerAchievementDate(testUUID, testAchievement);
+		assertNull(date);
 
-        registerAchievement();
+		registerAchievement();
 
-        date = db.getPlayerAchievementDate(testUUID, testAchievement);
-        assertNotNull(date);
-    }
+		date = db.getPlayerAchievementDate(testUUID, testAchievement);
+		assertNotNull(date);
+	}
 
-    @Test
-    public void testPlayerAchievementAmount() {
-        registerAchievement();
+	@Test
+	public void testPlayerAchievementAmount() {
+		registerAchievement();
 
-        assertEquals(1, db.getPlayerAchievementsAmount(testUUID));
-    }
+		assertEquals(1, db.getPlayerAchievementsAmount(testUUID));
+	}
 
-    @Test
-    public void testDeleteAchievement() {
-        testPlayerAchievementAmount();
+	@Test
+	public void testDeleteAchievement() {
+		testPlayerAchievementAmount();
 
-        db.deletePlayerAchievement(testUUID, testAchievement);
+		db.deletePlayerAchievement(testUUID, testAchievement);
 
-        assertEquals(0, db.getPlayerAchievementsAmount(testUUID));
-    }
+		assertEquals(0, db.getPlayerAchievementsAmount(testUUID));
+	}
 
-    @Test
-    public void testDeleteAchievementQuotes() {
-        registerAchievement(testUUID, "'" + testAchievement + "'", testAchievementMsg);
+	@Test
+	public void testDeleteAchievementQuotes() {
+		registerAchievement(testUUID, "'" + testAchievement + "'", testAchievementMsg);
 
-        registerAchievement(testUUID, "''" + testAchievement + "''", testAchievementMsg);
-        registerAchievement(testUUID, testAchievement, testAchievementMsg);
+		registerAchievement(testUUID, "''" + testAchievement + "''", testAchievementMsg);
+		registerAchievement(testUUID, testAchievement, testAchievementMsg);
 
-        assertEquals(3, db.getPlayerAchievementsAmount(testUUID));
+		assertEquals(3, db.getPlayerAchievementsAmount(testUUID));
 
-        db.deletePlayerAchievement(testUUID, "'" + testAchievement + "'");
-        db.deletePlayerAchievement(testUUID, testAchievement);
+		db.deletePlayerAchievement(testUUID, "'" + testAchievement + "'");
+		db.deletePlayerAchievement(testUUID, testAchievement);
 
-        assertEquals(0, db.getPlayerAchievementsAmount(testUUID));
-    }
+		assertEquals(0, db.getPlayerAchievementsAmount(testUUID));
+	}
 
-    @Test
-    public void testConnectionUpdate() {
-        assertEquals(0, db.getConnectionsAmount(testUUID));
+	@Test
+	public void testConnectionUpdate() {
+		assertEquals(0, db.getConnectionsAmount(testUUID));
 
-        assertEquals(1, db.updateAndGetConnection(testUUID, createDateString()));
-        assertEquals(2, db.updateAndGetConnection(testUUID, createDateString()));
-        assertEquals(3, db.updateAndGetConnection(testUUID, createDateString()));
+		assertEquals(1, db.updateAndGetConnection(testUUID, createDateString()));
+		assertEquals(2, db.updateAndGetConnection(testUUID, createDateString()));
+		assertEquals(3, db.updateAndGetConnection(testUUID, createDateString()));
 
-        assertEquals(3, db.getConnectionsAmount(testUUID));
-    }
+		assertEquals(3, db.getConnectionsAmount(testUUID));
+	}
 
-    @Test
-    public void testGetTopAchievements() {
-        long firstSave = 99L;
+	@Test
+	public void testGetTopAchievements() {
+		long firstSave = 99L;
 
-        System.out.println("Save first achievement:  " + System.currentTimeMillis());
-        registerAchievement(testUUID, testAchievement, testAchievementMsg, 100L);
+		System.out.println("Save first achievement:  " + System.currentTimeMillis());
+		registerAchievement(testUUID, testAchievement, testAchievementMsg, 100L);
 
-        long secondSave = 199L;
+		long secondSave = 199L;
 
-        UUID secondUUID = UUID.randomUUID();
-        String secondAch = "TestAchievement2";
+		UUID secondUUID = UUID.randomUUID();
+		String secondAch = "TestAchievement2";
 
-        System.out.println("Save second achievement: " + System.currentTimeMillis());
-        registerAchievement(secondUUID, testAchievement, testAchievementMsg, 200L);
-        System.out.println("Save third achievement:  " + System.currentTimeMillis());
-        registerAchievement(secondUUID, secondAch, testAchievementMsg, 200L);
+		System.out.println("Save second achievement: " + System.currentTimeMillis());
+		registerAchievement(secondUUID, testAchievement, testAchievementMsg, 200L);
+		System.out.println("Save third achievement:  " + System.currentTimeMillis());
+		registerAchievement(secondUUID, secondAch, testAchievementMsg, 200L);
 
-        Map<String, Integer> expected = new LinkedHashMap<>();
-        expected.put(secondUUID.toString(), 2);
-        expected.put(testUUID.toString(), 1);
+		Map<String, Integer> expected = new LinkedHashMap<>();
+		expected.put(secondUUID.toString(), 2);
+		expected.put(testUUID.toString(), 1);
 
-        Map<String, Integer> topList = db.getTopList(0);
-        assertEquals(expected, topList);
+		Map<String, Integer> topList = db.getTopList(0);
+		assertEquals(expected, topList);
 
-        Map<String, Integer> topListFirst = db.getTopList(firstSave);
-        assertEquals("Top list from first save & all top list should be the same",
-                topList, topListFirst);
+		Map<String, Integer> topListFirst = db.getTopList(firstSave);
+		assertEquals("Top list from first save & all top list should be the same",
+				topList, topListFirst);
 
-        expected.remove(testUUID.toString());
+		expected.remove(testUUID.toString());
 
-        Map<String, Integer> topListSecond = db.getTopList(secondSave);
-        assertEquals(expected, topListSecond);
-    }
+		Map<String, Integer> topListSecond = db.getTopList(secondSave);
+		assertEquals(expected, topListSecond);
+	}
 
-    @Test
-    public void testGetAchievementNameList() {
-        registerAchievement();
+	@Test
+	public void testGetAchievementNameList() {
+		registerAchievement();
 
-        List<String> expected = Collections.singletonList(testAchievement);
-        List<String> achNames = db.getPlayerAchievementNamesList(testUUID);
-        assertEquals(expected, achNames);
-    }
+		List<String> expected = Collections.singletonList(testAchievement);
+		List<String> achNames = db.getPlayerAchievementNamesList(testUUID);
+		assertEquals(expected, achNames);
+	}
 
-    @Test
-    public void testHasAchievement() {
-        assertFalse(db.hasPlayerAchievement(testUUID, testAchievement));
+	@Test
+	public void testHasAchievement() {
+		assertFalse(db.hasPlayerAchievement(testUUID, testAchievement));
 
-        registerAchievement();
+		registerAchievement();
 
-        assertTrue(db.hasPlayerAchievement(testUUID, testAchievement));
-    }
+		assertTrue(db.hasPlayerAchievement(testUUID, testAchievement));
+	}
 
-    @Test
-    public void testGetPlayerConnectionDate() {
-        assertNull(db.getPlayerConnectionDate(testUUID));
+	@Test
+	public void testGetPlayerConnectionDate() {
+		assertNull(db.getPlayerConnectionDate(testUUID));
 
-        db.updateAndGetConnection(testUUID, createDateString());
+		db.updateAndGetConnection(testUUID, createDateString());
 
-        assertNotNull(db.getPlayerConnectionDate(testUUID));
-    }
+		assertNotNull(db.getPlayerConnectionDate(testUUID));
+	}
 
-    @Test
-    public void testClearConnection() {
-        db.updateAndGetConnection(testUUID, createDateString());
+	@Test
+	public void testClearConnection() {
+		db.updateAndGetConnection(testUUID, createDateString());
 
-        assertNotNull(db.getPlayerConnectionDate(testUUID));
+		assertNotNull(db.getPlayerConnectionDate(testUUID));
 
-        db.clearConnection(testUUID);
+		db.clearConnection(testUUID);
 
-        assertNull(db.getPlayerConnectionDate(testUUID));
-    }
+		assertNull(db.getPlayerConnectionDate(testUUID));
+	}
 
-    private String createDateString() {
-        return new Date(System.currentTimeMillis()).toString();
-    }
+	private String createDateString() {
+		return new Date(System.currentTimeMillis()).toString();
+	}
 }
