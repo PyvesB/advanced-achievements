b3a78ab58e1f6603faf9f269fd3f6981299954e0
Rsl1122 <djrapitops@hotmail.com>
2018-01-08 14:52:52 +0200

    Lang implementation for GUI

--- src/main/java/com/hm/achievement/lang/GuiLang.java
+++ src/main/java/com/hm/achievement/lang/GuiLang.java
@@ -1,8 +1,21 @@
 package com.hm.achievement.lang;
 
 public enum GuiLang implements Lang {
-	LIST_ACHIEVEMENTS_IN_CATEGORY_SINGULAR("AMOUNT achievement"),
-	LIST_ACHIEVEMENTS_IN_CATEGORY_PLURAL("AMOUNT achievements");
+	ACHIEVEMENTS_IN_CATEGORY_SINGULAR("AMOUNT achievement"),
+	ACHIEVEMENTS_IN_CATEGORY_PLURAL("AMOUNT achievements"),
+	COMMANDS("Other Achievements"), 
+	GUI_TITLE("&5&lAchievements List"), 
+	ACHIEVEMENT_RECEIVED("&a\u2714&f "), 
+	ACHIEVEMENT_NOT_RECEIVED("&4\u2718 "), 
+	DESCRIPTION("Description:"), 
+	RECEPTION("Reception date:"), 
+	GOAL("Goal:"), 
+	PROGRESS("Progress:"), 
+	REWARD("Reward(s):"), 
+	PREVIOUS_MESSAGE("&7Previous"), 
+	NEXT_MESSAGE("&7Next"), 
+	BACK_MESSAGE("&7Back"), 
+	CATEGORY_NOT_UNLOCKED("You have not yet unlocked this category.");
 
 	private final String defaultMessage;
 
@@ -13,7 +26,7 @@ public enum GuiLang implements Lang {
 
 	@Override
 	public String toLangKey() {
-		return Lang.toPath(name());
+		return "list-" + Lang.toPath(name());
 	}
 
 	@Override
