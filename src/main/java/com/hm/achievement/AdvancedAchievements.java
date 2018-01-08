cd7d12d127d5d9c253af78a9d89a4f3658f2e79c
Rsl1122 <djrapitops@hotmail.com>
2018-01-08 10:56:49 +0200

    Split loadAndBackupFile into two smaller methods

--- src/main/java/com/hm/achievement/AdvancedAchievements.java
+++ src/main/java/com/hm/achievement/AdvancedAchievements.java
@@ -349,19 +349,28 @@ public class AdvancedAchievements extends JavaPlugin implements Reloadable {
 	 * @throws PluginLoadError
 	 */
 	public CommentedYamlConfiguration loadAndBackupFile(String fileName) throws PluginLoadError {
-		CommentedYamlConfiguration configFile = null;
 		getLogger().info("Loading and backing up " + fileName + " file...");
+
+		CommentedYamlConfiguration configFile = loadFile(fileName);
+		return backupConfig(configFile);
+	}
+
+	private CommentedYamlConfiguration loadFile(String fileName) throws PluginLoadError {
+		CommentedYamlConfiguration configFile;
 		try {
 			configFile = new CommentedYamlConfiguration(fileName, this);
 		} catch (IOException | InvalidConfigurationException e) {
 			throw new PluginLoadError("Error while loading " + fileName
 					+ ".Verify its syntax on yaml-online-parser.appspot.com and use the following logs", e);
 		}
+		return configFile;
+	}
 
+	private CommentedYamlConfiguration backupConfig(CommentedYamlConfiguration configFile) {
 		try {
 			configFile.backupConfiguration();
 		} catch (IOException e) {
-			getLogger().log(Level.SEVERE, "Error while backing up " + fileName + ".", e);
+			getLogger().log(Level.SEVERE, "Error while backing up " + configFile.getName() + ".", e);
 		}
 		return configFile;
 	}
