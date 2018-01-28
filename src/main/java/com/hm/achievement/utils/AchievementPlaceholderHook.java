e83263ddc6f338205bec32e7d94b2f9b8c730c17
Phoetrix <dylan.phoetrix@gmail.com>
2018-01-28 20:36:03 +0100

    Update AchievementPlaceholderHook.java

--- src/main/java/com/hm/achievement/utils/AchievementPlaceholderHook.java
+++ src/main/java/com/hm/achievement/utils/AchievementPlaceholderHook.java
@@ -27,27 +27,28 @@ public class AchievementPlaceholderHook extends EZPlaceholderHook {
 	@Override
 	public String onPlaceholderRequest(Player p, String identifier) {
 
-		int achievements = plugin.getCacheManager().getPlayerTotalAchievements(p.getUniqueId());
-
 		if (identifier.equalsIgnoreCase("achievements")) {
-			return String.valueOf(achievements);
+			return String.valueOf(plugin.getCacheManager().getPlayerTotalAchievements(p.getUniqueId()));
 		}
 
-		int totalAchievements = 0;
+		if (identifier.equalsIgnoreCase("achievements_percentage")) {
+			return String.format("%.1f",
+					100 * (double) plugin.getCacheManager().getPlayerTotalAchievements(p.getUniqueId())
+							/ plugin.getAchievementsAndDisplayNames().size())
+					+ "%";
+		}
+
+		if (identifier.equalsIgnoreCase("total_achievements")) {
+			return String.valueOf(plugin.getAchievementsAndDisplayNames().size());
+		}
 
 		for (NormalAchievements category : NormalAchievements.values()) {
 			String categoryName = category.toString();
-
-			if (plugin.getDisabledCategorySet().contains(categoryName)) {
-				continue;
-			}
 
 			if (identifier.equalsIgnoreCase(categoryName)) {
 				return String
 						.valueOf(plugin.getCacheManager().getAndIncrementStatisticAmount(category, p.getUniqueId(), 0));
 			}
-
-			totalAchievements += plugin.getPluginConfig().getConfigurationSection(categoryName).getKeys(false).size();
 		}
 
 		for (MultipleAchievements category : MultipleAchievements.values()) {
@@ -64,24 +65,8 @@ public class AchievementPlaceholderHook extends EZPlaceholderHook {
 					return String.valueOf(plugin.getCacheManager().getAndIncrementStatisticAmount(category, subcategory,
 							p.getUniqueId(), 0));
 				}
-
-				totalAchievements += plugin.getPluginConfig().getConfigurationSection(categoryName + '.' + subcategory)
-						.getKeys(false).size();
 			}
 		}
-
-		if (!plugin.getDisabledCategorySet().contains("Commands")) {
-			totalAchievements += plugin.getPluginConfig().getConfigurationSection("Commands").getKeys(false).size();
-		}
-
-		if (identifier.equalsIgnoreCase("achievements_percentage")) {
-			return String.format("%.1f", 100 * (double) achievements / totalAchievements) + "%";
-		}
-
-		if (identifier.equalsIgnoreCase("total_achievements")) {
-			return String.valueOf(totalAchievements);
-		}
-
 		return null;
 	}
 }
