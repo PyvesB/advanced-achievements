d108738be5528057213f1f08d2e571a67b16f4b8
Rsl1122 <djrapitops@hotmail.com>
2018-04-25 21:26:17 +0300

    Added args to Top-, Month-, & WeekCommands in PluginCommandExecutor

--- src/main/java/com/hm/achievement/command/PluginCommandExecutor.java
+++ src/main/java/com/hm/achievement/command/PluginCommandExecutor.java
@@ -1,11 +1,11 @@
 package com.hm.achievement.command;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
 
 /**
  * Class in charge of handling /aach commands and dispatching to the different command modules.
@@ -62,41 +62,42 @@ public class PluginCommandExecutor implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		// Map to an Advanced Achievements command.
-		if ((args.length == 1) && !"help".equalsIgnoreCase(args[0])) {
-			if ("book".equalsIgnoreCase(args[0])) {
+		String commandName = args[0];
+		if ((args.length == 1) && !"help".equalsIgnoreCase(commandName)) {
+			if ("book".equalsIgnoreCase(commandName)) {
 				bookCommand.executeCommand(sender, null, "book");
-			} else if ("hcaa".equalsIgnoreCase(args[0])) {
+			} else if ("hcaa".equalsIgnoreCase(commandName)) {
 				easterEggCommand.executeCommand(sender, null, "easteregg");
-			} else if ("reload".equalsIgnoreCase(args[0])) {
+			} else if ("reload".equalsIgnoreCase(commandName)) {
 				reloadCommand.executeCommand(sender, null, "reload");
-			} else if ("generate".equalsIgnoreCase(args[0])) {
+			} else if ("generate".equalsIgnoreCase(commandName)) {
 				generateCommand.executeCommand(sender, null, "generate");
-			} else if ("stats".equalsIgnoreCase(args[0])) {
+			} else if ("stats".equalsIgnoreCase(commandName)) {
 				statsCommand.executeCommand(sender, null, "stats");
-			} else if ("list".equalsIgnoreCase(args[0])) {
+			} else if ("list".equalsIgnoreCase(commandName)) {
 				listCommand.executeCommand(sender, null, "list");
-			} else if ("top".equalsIgnoreCase(args[0])) {
-				topCommand.executeCommand(sender, null, "top");
-			} else if ("week".equalsIgnoreCase(args[0])) {
-				weekCommand.executeCommand(sender, null, "week");
-			} else if ("month".equalsIgnoreCase(args[0])) {
-				monthCommand.executeCommand(sender, null, "month");
-			} else if ("info".equalsIgnoreCase(args[0])) {
+			} else if ("top".equalsIgnoreCase(commandName)) {
+				topCommand.executeCommand(sender, args, "top");
+			} else if ("week".equalsIgnoreCase(commandName)) {
+				weekCommand.executeCommand(sender, args, "week");
+			} else if ("month".equalsIgnoreCase(commandName)) {
+				monthCommand.executeCommand(sender, args, "month");
+			} else if ("info".equalsIgnoreCase(commandName)) {
 				infoCommand.executeCommand(sender, null, null);
-			} else if ("toggle".equalsIgnoreCase(args[0])) {
+			} else if ("toggle".equalsIgnoreCase(commandName)) {
 				toggleCommand.executeCommand(sender, null, "toggle");
 			} else {
 				helpCommand.executeCommand(sender, args, null);
 			}
-		} else if ((args.length == 3) && "reset".equalsIgnoreCase(args[0])) {
+		} else if ((args.length == 3) && "reset".equalsIgnoreCase(commandName)) {
 			resetCommand.executeCommand(sender, args, "reset");
-		} else if ((args.length == 3) && "give".equalsIgnoreCase(args[0])) {
+		} else if ((args.length == 3) && "give".equalsIgnoreCase(commandName)) {
 			giveCommand.executeCommand(sender, args, "give");
-		} else if ((args.length >= 3) && "check".equalsIgnoreCase(args[0])) {
+		} else if ((args.length >= 3) && "check".equalsIgnoreCase(commandName)) {
 			checkCommand.executeCommand(sender, args, "check");
-		} else if ((args.length >= 3) && "delete".equalsIgnoreCase(args[0])) {
+		} else if ((args.length >= 3) && "delete".equalsIgnoreCase(commandName)) {
 			deleteCommand.executeCommand(sender, args, "delete");
-		} else if ((args.length == 4) && "add".equalsIgnoreCase(args[0])) {
+		} else if ((args.length == 4) && "add".equalsIgnoreCase(commandName)) {
 			addCommand.executeCommand(sender, args, "add");
 		} else {
 			helpCommand.executeCommand(sender, args, null);
