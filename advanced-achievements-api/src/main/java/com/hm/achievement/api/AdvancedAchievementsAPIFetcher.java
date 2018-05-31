b7c8de5938dc9cf0d30b48e959cdbb8ccdcb0da5
PyvesB <PyvesDev@gmail.com>
2018-05-31 18:03:41 +0100

    Improved API fetcher class

--- advanced-achievements-api/src/main/java/com/hm/achievement/api/AdvancedAchievementsAPIFetcher.java
+++ advanced-achievements-api/src/main/java/com/hm/achievement/api/AdvancedAchievementsAPIFetcher.java
@@ -7,7 +7,10 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginLogger;
 
 /**
- * Class allowing developers to easily retrieve instances of the Advanced Achievements API.
+ * Class allowing developers to easily retrieve instances of the Advanced Achievements API. It is only shipped with
+ * plugin versions from 5.8.0 onwards; however, if shaded/copied within your own plugin, its methods will operate on
+ * plugin versions 5.7.0 onwards. Open an issue on GitHub (https://github.com/PyvesB/AdvancedAchievements/issues) if you
+ * need any help or have inquiries.
  * 
  * @author Pyves
  */
@@ -17,8 +20,7 @@ public class AdvancedAchievementsAPIFetcher {
 	private static final String API_GETTER = "getAdvancedAchievementsAPI";
 
 	/**
-	 * Retrieves an AdvancedAchievementsAPI instance that can be used to integrate with Advanced Achievements. This
-	 * methods will only work with plugin versions from 5.7.0 onwards.
+	 * Retrieves an AdvancedAchievementsAPI instance that can be used to integrate with Advanced Achievements.
 	 * 
 	 * @return an AdvancedAchievementsAPI instance wrapped in an optional or an empty optional if Advanced Achievements
 	 *         could not be linked
@@ -27,8 +29,9 @@ public class AdvancedAchievementsAPIFetcher {
 		Plugin plugin = Bukkit.getPluginManager().getPlugin("AdvancedAchievements");
 		if (plugin != null) {
 			try {
-				Object advancedAchievements = Class.forName(MAIN_CLASS).cast(plugin);
-				Object apiInstance = Class.forName(MAIN_CLASS).getMethod(API_GETTER).invoke(advancedAchievements);
+				Class<?> mainClass = Class.forName(MAIN_CLASS);
+				Object advancedAchievements = mainClass.cast(plugin);
+				Object apiInstance = mainClass.getMethod(API_GETTER).invoke(advancedAchievements);
 				return Optional.ofNullable((AdvancedAchievementsAPI) apiInstance);
 			} catch (Exception e) {
 				PluginLogger logger = new PluginLogger(plugin);
