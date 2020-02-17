23ab03031805c508a916815b1e0bd75165658929
PyvesB <PyvesDev@gmail.com>
2020-02-17 21:09:32 +0000

    Prevented exception when player is null (#665) and cleaned plaholder request handling

--- advanced-achievements-plugin/src/main/java/com/hm/achievement/placeholder/AchievementPlaceholderHook.java
+++ advanced-achievements-plugin/src/main/java/com/hm/achievement/placeholder/AchievementPlaceholderHook.java
@@ -1,16 +1,15 @@
 package com.hm.achievement.placeholder;
 
 import java.util.Map;
-import java.util.Set;
+import java.util.UUID;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
-import com.hm.achievement.AdvancedAchievements;
 import org.bukkit.entity.Player;
 
-import com.hm.achievement.category.Category;
+import com.hm.achievement.AdvancedAchievements;
 import com.hm.achievement.category.MultipleAchievements;
 import com.hm.achievement.category.NormalAchievements;
 import com.hm.achievement.db.CacheManager;
@@ -29,18 +28,15 @@ public class AchievementPlaceholderHook extends PlaceholderExpansion {
 	private final AdvancedAchievements advancedAchievements;
 	private final CommentedYamlConfiguration mainConfig;
 	private final CacheManager cacheManager;
-	private final Set<Category> disabledCategories;
 	private final Map<String, String> namesToDisplayNames;
 
 	@Inject
 	public AchievementPlaceholderHook(AdvancedAchievements advancedAchievements,
-			@Named("main") CommentedYamlConfiguration mainConfig,
-			CacheManager cacheManager, Set<Category> disabledCategories,
+			@Named("main") CommentedYamlConfiguration mainConfig, CacheManager cacheManager,
 			@Named("ntd") Map<String, String> namesToDisplayNames) {
 		this.advancedAchievements = advancedAchievements;
 		this.mainConfig = mainConfig;
 		this.cacheManager = cacheManager;
-		this.disabledCategories = disabledCategories;
 		this.namesToDisplayNames = namesToDisplayNames;
 	}
 
@@ -51,38 +47,33 @@ public class AchievementPlaceholderHook extends PlaceholderExpansion {
 
 	@Override
 	public String onPlaceholderRequest(Player p, String identifier) {
-		if ("achievements".equalsIgnoreCase(identifier)) {
-			return String.valueOf(cacheManager.getPlayerTotalAchievements(p.getUniqueId()));
-		}
-
-		if ("achievements_percentage".equalsIgnoreCase(identifier)) {
-			return String.format("%.1f%%", 100 * (double) cacheManager.getPlayerTotalAchievements(p.getUniqueId())
-					/ namesToDisplayNames.size());
-		}
-
 		if ("total_achievements".equalsIgnoreCase(identifier)) {
-			return String.valueOf(namesToDisplayNames.size());
+			return Integer.toString(namesToDisplayNames.size());
 		}
 
-		for (NormalAchievements category : NormalAchievements.values()) {
-			String categoryName = category.toString();
-
-			if (identifier.equalsIgnoreCase(categoryName)) {
-				return String.valueOf(cacheManager.getAndIncrementStatisticAmount(category, p.getUniqueId(), 0));
-			}
-		}
-
-		for (MultipleAchievements category : MultipleAchievements.values()) {
-			if (disabledCategories.contains(category)) {
-				continue;
+		if (p != null) {
+			UUID uuid = p.getUniqueId();
+			if ("achievements".equalsIgnoreCase(identifier)) {
+				return Integer.toString(cacheManager.getPlayerTotalAchievements(uuid));
 			}
 
-			for (String subcategory : mainConfig.getShallowKeys(category.toString())) {
-				String categoryPath = category + "_" + subcategory;
+			if ("achievements_percentage".equalsIgnoreCase(identifier)) {
+				return String.format("%.1f%%", 100 * (double) cacheManager.getPlayerTotalAchievements(uuid)
+						/ namesToDisplayNames.size());
+			}
 
-				if (identifier.equalsIgnoreCase(categoryPath)) {
-					return String
-							.valueOf(cacheManager.getAndIncrementStatisticAmount(category, subcategory, p.getUniqueId(), 0));
+			for (NormalAchievements category : NormalAchievements.values()) {
+				if (category.toString().equalsIgnoreCase(identifier)) {
+					return Long.toString(cacheManager.getAndIncrementStatisticAmount(category, uuid, 0));
+				}
+			}
+
+			for (MultipleAchievements category : MultipleAchievements.values()) {
+				for (String subcategory : mainConfig.getShallowKeys(category.toString())) {
+					String categoryPath = category + "_" + subcategory;
+					if (categoryPath.equalsIgnoreCase(identifier)) {
+						return Long.toString(cacheManager.getAndIncrementStatisticAmount(category, subcategory, uuid, 0));
+					}
 				}
 			}
 		}
