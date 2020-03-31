27a390333a1373d465453387df4db26271d5a91e
Beech Horn <beechhorn@gmail.com>
2020-03-31 10:25:27 +0100

    Process playtime achievement eligability in parallel to speed up when there are a large number of players.

--- advanced-achievements-plugin/src/main/java/com/hm/achievement/runnable/AchievePlayTimeRunnable.java
+++ advanced-achievements-plugin/src/main/java/com/hm/achievement/runnable/AchievePlayTimeRunnable.java
@@ -56,7 +56,7 @@ public class AchievePlayTimeRunnable extends StatisticIncreaseHandler implements
 	@Override
 	public void run() {
 		long currentTime = System.currentTimeMillis();
-		Bukkit.getOnlinePlayers().stream().forEach(p -> updateTime(p, currentTime));
+		Bukkit.getOnlinePlayers().parallelStream().forEach(p -> updateTime(p, currentTime));
 		previousRunMillis = currentTime;
 	}
 
