b62c1de98e8d71160ef681d2b8ecbc375337abe5
rafael59r2 <12960698+rafael59r2@users.noreply.github.com>
2020-04-22 15:47:40 +0100

    Update BrewingListener.java

--- advanced-achievements-plugin/src/main/java/com/hm/achievement/listener/statistics/BrewingListener.java
+++ advanced-achievements-plugin/src/main/java/com/hm/achievement/listener/statistics/BrewingListener.java
@@ -1,23 +1,5 @@
 package com.hm.achievement.listener.statistics;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Logger;
-
-import javax.inject.Inject;
-import javax.inject.Named;
-import javax.inject.Singleton;
-
-import org.bukkit.Material;
-import org.bukkit.entity.HumanEntity;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.inventory.*;
-import org.bukkit.inventory.BrewerInventory;
-import org.bukkit.inventory.ItemStack;
-
 import com.hm.achievement.AdvancedAchievements;
 import com.hm.achievement.category.NormalAchievements;
 import com.hm.achievement.db.CacheManager;
@@ -25,7 +7,22 @@ import com.hm.achievement.utils.InventoryHelper;
 import com.hm.achievement.utils.MaterialHelper;
 import com.hm.achievement.utils.RewardParser;
 import com.hm.mcshared.file.CommentedYamlConfiguration;
-import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.Material;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.inventory.BrewEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.BrewerInventory;
+import org.bukkit.inventory.ItemStack;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
 
 /**
  * Listener class to deal with Brewing achievements.
@@ -41,22 +38,23 @@ public class BrewingListener extends AbstractRateLimitedListener {
 
 	@Inject
 	public BrewingListener(@Named("main") CommentedYamlConfiguration mainConfig, int serverVersion,
-			Map<String, List<Long>> sortedThresholds, CacheManager cacheManager, RewardParser rewardParser,
-			AdvancedAchievements advancedAchievements, @Named("lang") CommentedYamlConfiguration langConfig, Logger logger,
-			MaterialHelper materialHelper, InventoryHelper inventoryHelper) {
+						   Map<String, List<Long>> sortedThresholds, CacheManager cacheManager, RewardParser rewardParser,
+						   AdvancedAchievements advancedAchievements, @Named("lang") CommentedYamlConfiguration langConfig, Logger logger,
+						   MaterialHelper materialHelper, InventoryHelper inventoryHelper) {
 		super(NormalAchievements.BREWING, mainConfig, serverVersion, sortedThresholds, cacheManager, rewardParser,
 				advancedAchievements, langConfig, logger);
 		this.materialHelper = materialHelper;
 		this.inventoryHelper = inventoryHelper;
 	}
+
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onBrew(BrewEvent event) {
-		for(HumanEntity he : event.getContents().getViewers()){
-			if(he instanceof Player){
-				Player p = (Player)he;
+		for (HumanEntity he : event.getContents().getViewers()) {
+			if (he instanceof Player) {
+				Player p = (Player) he;
 				BrewerInventory inv = event.getContents();
-				for (int i = 0; i <= 2; ++i) {
-					if(inv.getItem(i) == null) continue;
+				for (int i = 0; i <= 2; i++) {
+					if (inv.getItem(i) == null) continue;
 					Integer amount = inv.getItem(i).getAmount();
 					updateStatisticAndAwardAchievementsIfAvailable(p, amount, i);
 				}
