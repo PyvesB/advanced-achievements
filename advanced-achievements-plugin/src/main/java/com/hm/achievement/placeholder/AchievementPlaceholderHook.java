e97a337782b903e77e25417c3df18ec4f9e533b8
Arkobat <kr-jo@live.dk>
2020-07-14 22:53:00 +0200

    Unlocked aach placeholder
    Added placeholder to see if a player has unlocked a specific achievement
    Refactored placeholders into own method

--- advanced-achievements-plugin/src/main/java/com/hm/achievement/placeholder/AchievementPlaceholderHook.java
+++ advanced-achievements-plugin/src/main/java/com/hm/achievement/placeholder/AchievementPlaceholderHook.java
@@ -1,100 +1,132 @@
 package com.hm.achievement.placeholder;
 
-import java.util.Map;
-import java.util.UUID;
-
-import javax.inject.Inject;
-import javax.inject.Named;
-import javax.inject.Singleton;
-
-import org.bukkit.entity.Player;
-
 import com.hm.achievement.AdvancedAchievements;
 import com.hm.achievement.category.MultipleAchievements;
 import com.hm.achievement.category.NormalAchievements;
 import com.hm.achievement.db.CacheManager;
 import com.hm.mcshared.file.CommentedYamlConfiguration;
-
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.entity.Player;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * Class enabling usage of placeholder with PlaceholderAPI to get achievements stats in others plugins.
- * 
+ *
  * @author Phoetrix
  */
 @Singleton
 public class AchievementPlaceholderHook extends PlaceholderExpansion {
 
-	private final AdvancedAchievements advancedAchievements;
-	private final CommentedYamlConfiguration mainConfig;
-	private final CacheManager cacheManager;
-	private final Map<String, String> namesToDisplayNames;
+    private final AdvancedAchievements advancedAchievements;
+    private final CommentedYamlConfiguration mainConfig;
+    private final CacheManager cacheManager;
+    private final Map<String, String> namesToDisplayNames;
 
-	@Inject
-	public AchievementPlaceholderHook(AdvancedAchievements advancedAchievements,
-			@Named("main") CommentedYamlConfiguration mainConfig, CacheManager cacheManager,
-			@Named("ntd") Map<String, String> namesToDisplayNames) {
-		this.advancedAchievements = advancedAchievements;
-		this.mainConfig = mainConfig;
-		this.cacheManager = cacheManager;
-		this.namesToDisplayNames = namesToDisplayNames;
-	}
+    @Inject
+    public AchievementPlaceholderHook(AdvancedAchievements advancedAchievements,
+                                      @Named("main") CommentedYamlConfiguration mainConfig, CacheManager cacheManager,
+                                      @Named("ntd") Map<String, String> namesToDisplayNames) {
+        this.advancedAchievements = advancedAchievements;
+        this.mainConfig = mainConfig;
+        this.cacheManager = cacheManager;
+        this.namesToDisplayNames = namesToDisplayNames;
+    }
 
-	@Override
-	public boolean persist() {
-		return true;
-	}
+    @Override
+    public boolean persist() {
+        return true;
+    }
 
-	@Override
-	public String onPlaceholderRequest(Player p, String identifier) {
-		if ("total_achievements".equalsIgnoreCase(identifier)) {
-			return Integer.toString(namesToDisplayNames.size());
-		}
+    @Override
+    public String onPlaceholderRequest(Player p, String identifier) {
+        if (identifier.equalsIgnoreCase("total_achievements")) {
+            return getTotalAchievements();
+        }
 
-		if (p != null) {
-			UUID uuid = p.getUniqueId();
-			if ("achievements".equalsIgnoreCase(identifier)) {
-				return Integer.toString(cacheManager.getPlayerTotalAchievements(uuid));
-			}
+        if (p == null) {
+            return null;
+        }
 
-			if ("achievements_percentage".equalsIgnoreCase(identifier)) {
-				return String.format("%.1f%%", 100 * (double) cacheManager.getPlayerTotalAchievements(uuid)
-						/ namesToDisplayNames.size());
-			}
+        UUID uuid = p.getUniqueId();
 
-			for (NormalAchievements category : NormalAchievements.values()) {
-				if (category.toString().equalsIgnoreCase(identifier)) {
-					long statistic = cacheManager.getAndIncrementStatisticAmount(category, uuid, 0);
-					// If played time, convert from millis to hours and display one decimal.
-					return category == NormalAchievements.PLAYEDTIME ? String.format("%.1f", statistic / 3600000.0)
-							: Long.toString(statistic);
-				}
-			}
+        if (identifier.equalsIgnoreCase("achievements")) {
+            return getAchievements(uuid);
+        }
 
-			for (MultipleAchievements category : MultipleAchievements.values()) {
-				for (String subcategory : mainConfig.getShallowKeys(category.toString())) {
-					String categoryPath = category + "_" + subcategory;
-					if (categoryPath.equalsIgnoreCase(identifier)) {
-						return Long.toString(cacheManager.getAndIncrementStatisticAmount(category, subcategory, uuid, 0));
-					}
-				}
-			}
-		}
-		return null;
-	}
+        if (identifier.equalsIgnoreCase("achievements_percentage")) {
+            return getAchievementsPercentage(uuid);
+        }
 
-	@Override
-	public String getIdentifier() {
-		return "aach";
-	}
+        if (identifier.toLowerCase().startsWith("unlocked_")) {
+            return unlockedPlaceholder(uuid, identifier.replaceFirst("(?i)unlocked_", ""));
+        }
 
-	@Override
-	public String getAuthor() {
-		return String.join(", ", advancedAchievements.getDescription().getAuthors());
-	}
+        String returnValue = getNormalAchievements(identifier, uuid);
+        if (returnValue == null) {
+            returnValue = getMultipleAchievements(identifier, uuid);
+        }
 
-	@Override
-	public String getVersion() {
-		return advancedAchievements.getDescription().getVersion();
-	}
+        return returnValue;
+    }
+
+    private String getTotalAchievements() {
+        return Integer.toString(namesToDisplayNames.size());
+    }
+
+    private String getAchievements(UUID uuid) {
+        return Integer.toString(cacheManager.getPlayerTotalAchievements(uuid));
+    }
+
+    private String getAchievementsPercentage(UUID uuid) {
+        return String.format("%.1f%%", 100 * (double) cacheManager.getPlayerTotalAchievements(uuid)
+                / namesToDisplayNames.size());
+    }
+
+    private String unlockedPlaceholder(UUID uuid, String name) {
+        return Boolean.toString(cacheManager.hasPlayerAchievement(uuid, name));
+    }
+
+    private String getNormalAchievements(String identifier, UUID uuid) {
+        for (NormalAchievements category : NormalAchievements.values()) {
+            if (identifier.equalsIgnoreCase(category.toString())) {
+                long statistic = cacheManager.getAndIncrementStatisticAmount(category, uuid, 0);
+                // If played time, convert from millis to hours and display one decimal.
+                return category == NormalAchievements.PLAYEDTIME ? String.format("%.1f", statistic / 3600000.0)
+                        : Long.toString(statistic);
+            }
+        }
+        return null;
+    }
+
+    private String getMultipleAchievements(String identifier, UUID uuid) {
+        for (MultipleAchievements category : MultipleAchievements.values()) {
+            for (String subcategory : mainConfig.getShallowKeys(category.toString())) {
+                String categoryPath = category + "_" + subcategory;
+                if (identifier.equalsIgnoreCase(categoryPath)) {
+                    return Long.toString(cacheManager.getAndIncrementStatisticAmount(category, subcategory, uuid, 0));
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "aach";
+    }
+
+    @Override
+    public String getAuthor() {
+        return String.join(", ", advancedAchievements.getDescription().getAuthors());
+    }
+
+    @Override
+    public String getVersion() {
+        return advancedAchievements.getDescription().getVersion();
+    }
 }
