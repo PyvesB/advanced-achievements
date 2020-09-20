899c5c47e88374b58880ca125447c3807eb96746
PyvesB <PyvesDev@gmail.com>
2020-09-20 12:40:53 +0100

    Extend /aach list to allow opening categories directly (#694)

--- advanced-achievements-plugin/src/main/java/com/hm/achievement/command/executable/ListCommand.java
+++ advanced-achievements-plugin/src/main/java/com/hm/achievement/command/executable/ListCommand.java
@@ -1,15 +1,28 @@
 package com.hm.achievement.command.executable;
 
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import com.hm.achievement.lang.GuiLang;
 import com.hm.achievement.lang.LangHelper;
+import com.hm.achievement.lang.command.CmdLang;
+import com.hm.achievement.utils.StringHelper;
+
+import org.apache.commons.lang3.StringUtils;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
+import com.hm.achievement.gui.CategoryGUI;
+import com.hm.achievement.gui.GUIItems;
 import com.hm.achievement.gui.MainGUI;
+import com.hm.achievement.gui.OrderedCategory;
 import com.hm.mcshared.file.CommentedYamlConfiguration;
 
 /**
@@ -18,16 +31,30 @@ import com.hm.mcshared.file.CommentedYamlConfiguration;
  * @author Pyves
  */
 @Singleton
-@CommandSpec(name = "list", permission = "list", minArgs = 1, maxArgs = 1)
+@CommandSpec(name = "list", permission = "list", minArgs = 1, maxArgs = 2)
 public class ListCommand extends AbstractCommand {
 
 	private final MainGUI mainGUI;
+	private final CategoryGUI categoryGUI;
+	private final GUIItems guiItems;
+
+	private String langCategoryDoesNotExist;
 
 	@Inject
 	public ListCommand(@Named("main") CommentedYamlConfiguration mainConfig,
-			@Named("lang") CommentedYamlConfiguration langConfig, StringBuilder pluginHeader, MainGUI mainGUI) {
+			@Named("lang") CommentedYamlConfiguration langConfig, StringBuilder pluginHeader, MainGUI mainGUI,
+			CategoryGUI categoryGUI, GUIItems guiItems) {
 		super(mainConfig, langConfig, pluginHeader);
 		this.mainGUI = mainGUI;
+		this.categoryGUI = categoryGUI;
+		this.guiItems = guiItems;
+	}
+
+	@Override
+	public void extractConfigurationParameters() {
+		super.extractConfigurationParameters();
+
+		langCategoryDoesNotExist = pluginHeader + LangHelper.get(CmdLang.CATEGORY_DOES_NOT_EXIST, langConfig);
 	}
 
 	@Override
@@ -43,6 +70,24 @@ public class ListCommand extends AbstractCommand {
 			return;
 		}
 
-		mainGUI.displayMainGUI(player);
+		if (args.length == 1) {
+			mainGUI.displayMainGUI(player);
+		} else {
+			String categoryName = args[1];
+			Optional<Entry<OrderedCategory, ItemStack>> matchingCategory = guiItems.getOrderedAchievementItems().entrySet()
+					.stream()
+					.filter(e -> e.getKey().getCategory().toString().equals(categoryName))
+					.findFirst();
+			if (matchingCategory.isPresent()) {
+				categoryGUI.displayCategoryGUI(matchingCategory.get().getValue(), player, 0);
+			} else {
+				List<String> allGUICategoryNames = guiItems.getOrderedAchievementItems().keySet()
+						.stream()
+						.map(c -> c.getCategory().toString())
+						.collect(Collectors.toList());
+				sender.sendMessage(StringUtils.replaceEach(langCategoryDoesNotExist, new String[] { "CAT", "CLOSEST_MATCH" },
+						new String[] { categoryName, StringHelper.getClosestMatch(categoryName, allGUICategoryNames) }));
+			}
+		}
 	}
 }
