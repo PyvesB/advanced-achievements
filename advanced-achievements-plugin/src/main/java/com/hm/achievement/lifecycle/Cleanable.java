86184c8e5ebdfb009ee99696a5487d20578cc483
PyvesB <PyvesDev@gmail.com>
2020-09-27 11:50:19 +0100

    Remove useless Observer parent

--- advanced-achievements-plugin/src/main/java/com/hm/achievement/lifecycle/Cleanable.java
+++ advanced-achievements-plugin/src/main/java/com/hm/achievement/lifecycle/Cleanable.java
@@ -1,22 +1,14 @@
 package com.hm.achievement.lifecycle;
 
-import java.util.Observable;
-import java.util.Observer;
 import java.util.UUID;
 
 /**
  * Interface used for classes that contain player specific data structures, and that should clean up when a player
- * disconnects. This avoids Maps and Sets from growing unboundedly until the server is restarted. Implementing classes
- * cooperate with QuitListener using the Observer pattern.
+ * disconnects (QuitListener). This avoids Maps and Sets from growing unboundedly until the server is restarted.
  *
  * @author Pyves
  */
-public interface Cleanable extends Observer {
-
-	@Override
-	default void update(Observable o, Object arg) {
-		cleanPlayerData((UUID) arg);
-	}
+public interface Cleanable {
 
 	/**
 	 * Cleans data for a specific player that has disconnected recently.
