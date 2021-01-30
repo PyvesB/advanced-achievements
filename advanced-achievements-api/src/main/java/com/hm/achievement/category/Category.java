ae25d08c972f5b1cfdbaf210069003ac17b67936
PyvesB <PyvesDev@gmail.com>
2021-01-30 15:38:07 +0000

    Remove deprecated interface methods

--- advanced-achievements-api/src/main/java/com/hm/achievement/category/Category.java
+++ advanced-achievements-api/src/main/java/com/hm/achievement/category/Category.java
@@ -29,37 +29,4 @@ public interface Category {
 	default String toChildPermName(String child) {
 		return toPermName();
 	}
-
-	/**
-	 * Converts to the comment that is inserted about the category name in the configuration file.
-	 * 
-	 * @deprecated category comments should be extracted from the default configuration files instead.
-	 * @return the configuration comment
-	 */
-	@Deprecated
-	default String toConfigComment() {
-		return "";
-	}
-
-	/**
-	 * Converts to the key in the language file.
-	 *
-	 * @deprecated this is effectively an internal implementation detail.
-	 * @return the language configuration key
-	 */
-	@Deprecated
-	default String toLangKey() {
-		return "";
-	}
-
-	/**
-	 * Converts to the default message that is returned, if key is not found in the configuration.
-	 *
-	 * @deprecated language default values should be extracted from the default configuration files instead.
-	 * @return the language configuration default value
-	 */
-	@Deprecated
-	default String toLangDefault() {
-		return "";
-	}
 }
