19a90ba0bd2a2d912157215f7684cc62b7b01715
PyvesB <PyvesDev@gmail.com>
2021-02-13 11:43:18 +0000

    Simplify Trades conditional

--- advanced-achievements-plugin/src/main/java/com/hm/achievement/listener/statistics/TradesListener.java
+++ advanced-achievements-plugin/src/main/java/com/hm/achievement/listener/statistics/TradesListener.java
@@ -4,7 +4,6 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
-import org.bukkit.Material;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,7 +12,6 @@ import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.inventory.ItemStack;
 
 import com.hm.achievement.category.NormalAchievements;
 import com.hm.achievement.config.AchievementMap;
@@ -36,12 +34,10 @@ public class TradesListener extends AbstractListener {
 
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onInventoryClick(InventoryClickEvent event) {
-		ItemStack item = event.getCurrentItem();
-		if (event.getRawSlot() != 2 || event.getInventory().getType() != InventoryType.MERCHANT || item == null
-				|| item.getType() == Material.AIR || event.getAction() == InventoryAction.NOTHING
-				|| event.getClick() == ClickType.NUMBER_KEY && event.getAction() == InventoryAction.HOTBAR_MOVE_AND_READD) {
-			return;
+		if ((event.getClick() != ClickType.NUMBER_KEY || event.getAction() != InventoryAction.HOTBAR_MOVE_AND_READD)
+				&& event.getRawSlot() == 2 && event.getInventory().getType() == InventoryType.MERCHANT
+				&& event.getAction() != InventoryAction.NOTHING) {
+			updateStatisticAndAwardAchievementsIfAvailable((Player) event.getWhoClicked(), 1);
 		}
-		updateStatisticAndAwardAchievementsIfAvailable((Player) event.getWhoClicked(), 1);
 	}
 }
