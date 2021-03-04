ce5d3f0c5e7b236ff4a20124407e2b7ae53c917b
PyvesB <PyvesDev@gmail.com>
2021-03-04 19:52:53 +0000

    Remove Connections caching
    This could cause a user to not receive Connections
    achievements/statistic increases when reconnecting to the server within
    the cache period.

--- advanced-achievements-plugin/src/main/java/com/hm/achievement/listener/statistics/ConnectionsListener.java
+++ advanced-achievements-plugin/src/main/java/com/hm/achievement/listener/statistics/ConnectionsListener.java
@@ -2,9 +2,6 @@ package com.hm.achievement.listener.statistics;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -24,7 +21,6 @@ import com.hm.achievement.category.NormalAchievements;
 import com.hm.achievement.config.AchievementMap;
 import com.hm.achievement.db.AbstractDatabaseManager;
 import com.hm.achievement.db.CacheManager;
-import com.hm.achievement.lifecycle.Cleanable;
 import com.hm.achievement.utils.PlayerAdvancedAchievementEvent;
 
 /**
@@ -35,11 +31,10 @@ import com.hm.achievement.utils.PlayerAdvancedAchievementEvent;
  *
  */
 @Singleton
-public class ConnectionsListener extends AbstractListener implements Cleanable {
+public class ConnectionsListener extends AbstractListener {
 
 	private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-	private final Set<UUID> playersConnectionProcessed = new HashSet<>();
 	private final AdvancedAchievements advancedAchievements;
 	private final AbstractDatabaseManager databaseManager;
 
@@ -49,11 +44,6 @@ public class ConnectionsListener extends AbstractListener implements Cleanable {
 		super(NormalAchievements.CONNECTIONS, mainConfig, serverVersion, achievementMap, cacheManager);
 		this.advancedAchievements = advancedAchievements;
 		this.databaseManager = databaseManager;
-	}
-
-	@Override
-	public void cleanPlayerData(UUID uuid) {
-		playersConnectionProcessed.remove(uuid);
 	}
 
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
@@ -77,35 +67,18 @@ public class ConnectionsListener extends AbstractListener implements Cleanable {
 	 * @param player
 	 */
 	private void scheduleAwardConnection(Player player) {
-		if (!playersConnectionProcessed.contains(player.getUniqueId())) {
-			Bukkit.getScheduler().scheduleSyncDelayedTask(advancedAchievements, () -> {
-				// In addition to the usual reception conditions, check that the player is still connected and that
-				// another runnable hasn't already done the work (even though this method is intended to run once per
-				// player per connection instance, it might happen with some server settings).
-				if (shouldIncreaseBeTakenIntoAccount(player, category) && player.isOnline()
-						&& !playersConnectionProcessed.contains(player.getUniqueId())) {
-					handleConnectionAchievements(player);
-					// Ran successfully to completion: no need to re-run while player is connected.
-					playersConnectionProcessed.add(player.getUniqueId());
+		Bukkit.getScheduler().scheduleSyncDelayedTask(advancedAchievements, () -> {
+			if (shouldIncreaseBeTakenIntoAccount(player, category) && player.isOnline()) {
+				String dateString = LocalDate.now().format(DATE_TIME_FORMATTER);
+				if (!dateString.equals(databaseManager.getPlayerConnectionDate(player.getUniqueId()))) {
+					long connections = databaseManager.updateAndGetConnection(player.getUniqueId(), dateString);
+					achievementMap.getForCategory(NormalAchievements.CONNECTIONS).stream()
+							.filter(achievement -> achievement.getThreshold() == connections)
+							.filter(achievement -> player.hasPermission("achievement." + achievement.getName()))
+							.forEach(achievement -> Bukkit.getPluginManager()
+									.callEvent(new PlayerAdvancedAchievementEvent(player, achievement)));
 				}
-			}, 100);
-		}
-	}
-
-	/**
-	 * Updates Connection statistics and awards an achievement if need-be.
-	 * 
-	 * @param player
-	 */
-	private void handleConnectionAchievements(Player player) {
-		String dateString = LocalDate.now().format(DATE_TIME_FORMATTER);
-		if (!dateString.equals(databaseManager.getPlayerConnectionDate(player.getUniqueId()))) {
-			long connections = databaseManager.updateAndGetConnection(player.getUniqueId(), dateString);
-			achievementMap.getForCategory(NormalAchievements.CONNECTIONS).stream()
-					.filter(achievement -> achievement.getThreshold() == connections)
-					.filter(achievement -> player.hasPermission("achievement." + achievement.getName()))
-					.forEach(achievement -> Bukkit.getPluginManager()
-							.callEvent(new PlayerAdvancedAchievementEvent(player, achievement)));
-		}
+			}
+		}, 100);
 	}
 }
