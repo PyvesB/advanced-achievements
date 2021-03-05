PyvesB/advanced-achievements

commit 3f6bd764e016317881f1f4a7abd47518493ac871

Remove unnecessary Singleton annotations

advanced-achievements-plugin/src/main/java/com/hm/achievement/lifecycle/PluginLoader.java
@@ -5,7 +5,6 @@ import java.util.logging.Logger;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.inject.Singleton;
 
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
@@ -52,7 +51,6 @@ import dagger.Lazy;
  *
  * @author Pyves
  */
-@Singleton
 public class PluginLoader {
 
 	private final AdvancedAchievements advancedAchievements;

advanced-achievements-plugin/src/main/java/com/hm/achievement/command/completer/CommandTabCompleter.java
@@ -8,7 +8,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
-import javax.inject.Singleton;
 
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.command.Command;
@@ -31,7 +30,6 @@ import com.hm.achievement.config.AchievementMap;
  *
  * @author Pyves
  */
-@Singleton
 public class CommandTabCompleter implements TabCompleter {
 
 	private static final int MAX_LIST_LENGTH = 50;

advanced-achievements-plugin/src/main/java/com/hm/achievement/config/ConfigurationParser.java
@@ -15,7 +15,6 @@ import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.inject.Singleton;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.text.StringEscapeUtils;
@@ -43,7 +42,6 @@ import com.hm.achievement.utils.StringHelper;
  *
  * @author Pyves
  */
-@Singleton
 public class ConfigurationParser {
 
 	private final YamlConfiguration mainConfig;

advanced-achievements-plugin/src/main/java/com/hm/achievement/config/YamlUpdater.java
@@ -14,7 +14,6 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.inject.Inject;
-import javax.inject.Singleton;
 
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.configuration.InvalidConfigurationException;
@@ -22,7 +21,6 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 import com.hm.achievement.AdvancedAchievements;
 
-@Singleton
 public class YamlUpdater {
 
 	private final AdvancedAchievements plugin;

advanced-achievements-plugin/src/main/java/com/hm/achievement/db/AsyncCachedRequestsSender.java
@@ -11,7 +11,6 @@ import java.util.UUID;
 import java.util.logging.Logger;
 
 import javax.inject.Inject;
-import javax.inject.Singleton;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -24,7 +23,6 @@ import com.hm.achievement.category.NormalAchievements;
  * @author Pyves
  *
  */
-@Singleton
 public class AsyncCachedRequestsSender implements Runnable {
 
 	private final Logger logger;

advanced-achievements-plugin/src/main/java/com/hm/achievement/listener/ListGUIListener.java
@@ -7,7 +7,6 @@ import java.util.Set;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.inject.Singleton;
 
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
@@ -33,7 +32,6 @@ import com.hm.achievement.gui.MainGUI;
  *
  * @author Pyves
  */
-@Singleton
 public class ListGUIListener implements Listener {
 
 	private final YamlConfiguration mainConfig;

advanced-achievements-plugin/src/main/java/com/hm/achievement/listener/TeleportListener.java
@@ -1,7 +1,6 @@
 package com.hm.achievement.listener;
 
 import javax.inject.Inject;
-import javax.inject.Singleton;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -18,7 +17,6 @@ import com.hm.achievement.runnable.AchieveDistanceRunnable;
  * @author Pyves
  *
  */
-@Singleton
 public class TeleportListener implements Listener {
 
 	private final AchieveDistanceRunnable distanceRunnable;

advanced-achievements-plugin/src/main/java/com/hm/achievement/listener/UpdateChecker.java
@@ -7,7 +7,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 import javax.inject.Inject;
-import javax.inject.Singleton;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -29,7 +28,6 @@ import com.hm.achievement.AdvancedAchievements;
  * 
  * @author Pyves
  */
-@Singleton
 public class UpdateChecker implements Listener {
 
 	private final AdvancedAchievements plugin;

commit a658251c190811c85fdda53751a682da1869eecc

Ensure only one PluginLoader instance is created
Regression from commit 3f6bd764e016317881f1f4a7abd47518493ac871

advanced-achievements-plugin/src/main/java/com/hm/achievement/lifecycle/PluginLoader.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.inject.Singleton;
 
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
@@ -50,6 +51,7 @@ import dagger.Lazy;
  *
  * @author Pyves
  */
+@Singleton
 public class PluginLoader {
 
 	private final AdvancedAchievements advancedAchievements;